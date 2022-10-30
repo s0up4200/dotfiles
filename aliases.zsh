c799e339b0e1bf87c51d862c5d35cfd13a187db8
soup <soup@r4tio.cat>
2022-10-30 18:56:13 +0100

    alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,7 @@ PROMPT="$fg[cyan]%}$USER@%{$fg[blue]%}%m ${PROMPT}"
 
 #ip stuff
 alias ips="/sbin/ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+alias vpn="curl https://am.i.mullvad.net/connected"
 
 #etc
 alias brr="vnstat -i $(/usr/sbin/route | grep '^default' | grep -o '[^ ]*$') -l"
@@ -11,4 +12,4 @@ alias grab-new="~/scripts/grab-new.sh"
 alias tagjob="qbittools tagging --added-on --not-working --tracker-down --unregistered --last-activity"
 
 ##ffsend https://github.com/timvisee/ffsend
-upload='ffsend upload --host https://send.domain.com'
+upload='ffsend upload --host https://send.r4tio.cat'
