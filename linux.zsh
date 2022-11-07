034221e61c2064d48eb711872b78ffd5f4716126
soup <soup@r4tio.cat>
2022-11-07 19:54:16 +0100

    pacman

--- linux.zsh
+++ linux.zsh
@@ -4,6 +4,11 @@
 #alias grab-new="~/scripts/grab-new.sh"
 #alias tagjob="qbittools tagging --added-on --not-working --tracker-down --unregistered --last-activity"
 
+
+#pacman
+alias pacman-upgrade="sudo pacman -Syu"     # Synchronize with repositories and then upgrade packages that are out of date on the local system.
+alias pacman-install="sudo pacman -S"        # Install specific package(s) from the repositories
+
 # show username with oh-my-zsh
 #PROMPT="$fg[cyan]%}$USER@%{$fg[blue]%}%m ${PROMPT}"
 
