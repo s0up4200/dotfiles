0991615fd2145a443b741a5b8b4a6b8336fcad32
soup <soup@r4tio.cat>
2022-11-13 12:39:27 +0100

    .

--- aliases.zsh
+++ aliases.zsh
@@ -3,7 +3,10 @@ PROMPT="$fg[cyan]%}$USER@%{$fg[blue]%}%m ${PROMPT}"
 
 #ip stuff
 alias ips="/sbin/ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
-alias vpn="curl https://am.i.mullvad.net/connected"
+alias vpn="curl https://am.i.mullvad.net/ip && curl https://am.i.mullvad.net/country"
 
 ##ffsend https://github.com/timvisee/ffsend
 alias upload="/opt/homebrew/bin/ffsend upload --host https://send.r4tio.cat"
+
+# clear
+alias clr="clear"
