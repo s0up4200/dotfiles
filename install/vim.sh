f88ba2cd9537da83f66ee642c7d878e91a61b88d
s0up4200 <s0up4200@pm.me>
2025-01-22 13:02:14 +0100

    Refactor vim configuration and plugin management
    - Updated .vimrc to include multiple color scheme plugins and enabled syntax highlighting.
    - Removed redundant settings and organized the vimrc for better readability.
    - Modified install/vim.sh to create a streamlined vimrc with essential plugin setup and configuration.
    
    This commit enhances the vim setup process by improving plugin management and simplifying the configuration file.

--- install/vim.sh
+++ install/vim.sh
@@ -24,53 +24,12 @@ fi
 
 # create vimrc if it doesn't exist
 cat > ~/.vimrc << 'EOL'
-" First, your plugin section
 call plug#begin('~/.vim/plugged')
 Plug 'morhetz/gruvbox'
 Plug 'dracula/vim'
 Plug 'joshdick/onedark.vim'
 Plug 'catppuccin/vim', { 'as': 'catppuccin' }
 call plug#end()
-
-" enable syntax highlighting
-syntax enable
-
-" enable true colors
-set termguicolors
-
-" set color scheme
-colorscheme catppuccin_macchiato
-
-" show line numbers
-set number
-
-" enable mouse support
-set mouse=a
-
-" set tab width to 4 spaces
-set tabstop=4
-set shiftwidth=4
-set expandtab
-
-" enable auto-indentation
-set autoindent
-set smartindent
-
-" highlight current line
-set cursorline
-
-" show matching brackets
-set showmatch
-
-" enable incremental search
-set incsearch
-set hlsearch
-
-" always show status line
-set laststatus=2
-
-" enable file type detection
-filetype plugin indent on
 EOL
 
 # Install plugins
