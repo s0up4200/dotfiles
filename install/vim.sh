f7eab13ea52fb10b4c8ebffeabe4d3788b775782
s0up4200 <s0up4200@pm.me>
2025-01-22 11:31:47 +0100

    Enhance vim installation script with configuration setup
    - Added echo statements to inform users about the setup process.
    - Changed the cloning path for the catppuccin theme to a temporary directory.
    - Implemented a new vimrc creation process with essential settings for syntax highlighting, color scheme, line numbers, mouse support, and indentation.
    - Cleaned up by removing the temporary directory after copying color files.
    
    This commit improves the vim setup experience by providing a complete configuration and clearer user instructions.

--- install/vim.sh
+++ install/vim.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
+echo "Setting up vim configuration..."
+
 # create required directories
 mkdir -p ~/.vim/colors
 
 # clone catppuccin theme
-git clone https://github.com/catppuccin/vim.git ~/.vim/catppuccin-vim
+git clone https://github.com/catppuccin/vim.git /tmp/catppuccin-vim
 
 # copy color files
-cp ~/.vim/catppuccin-vim/colors/* ~/.vim/colors/
+cp /tmp/catppuccin-vim/colors/* ~/.vim/colors/
 
 # cleanup downloaded repo
-rm -rf ~/.vim/catppuccin-vim
+rm -rf /tmp/catppuccin-vim
 
-# copy vimrc file
-cp .vim/vimrc ~/.vim/vimrc
+# create vimrc if it doesn't exist
+cat > ~/.vimrc << 'EOL'
+" enable syntax highlighting
+syntax on
 
-# create symlink to .vimrc in home directory
-ln -sf ~/.vim/vimrc ~/.vimrc
+" set color scheme
+colorscheme catppuccin_mocha
 
-chmod +x install/vim.sh 
+" show line numbers
+set number
+
+" enable mouse support
+set mouse=a
+
+" set tab width to 4 spaces
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+" enable auto-indentation
+set autoindent
+set smartindent
+
+" highlight current line
+set cursorline
+
+" show matching brackets
+set showmatch
+
+" enable incremental search
+set incsearch
+set hlsearch
+
+" always show status line
+set laststatus=2
+
+" enable file type detection
+filetype plugin indent on
+EOL
+
+echo "Vim configuration complete!" 
